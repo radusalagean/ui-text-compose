83e9485c52b26915d928a24e774c8e4012298aca
Radu Salagean <radusalagean@outlook.com>
2025-05-14 15:17:40 +0300

    Integrated Kotlin binary-compatibility-validator

--- uitextcompose-android/build.gradle.kts
+++ uitextcompose-android/build.gradle.kts
@@ -3,6 +3,7 @@ import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.binaryCompatibilityValidator)
     alias(libs.plugins.android.library)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.vanniktech.mavenPublish)
--- uitextcompose-multiplatform/build.gradle.kts
+++ uitextcompose-multiplatform/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.binaryCompatibilityValidator)
     alias(libs.plugins.android.library)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.compose.multiplatform)
